0b2963f28fb2bae10a68c1f5d26e95008c791974
Natesworks <121343177+natesworks@users.noreply.github.com>
2023-08-15 10:59:11 +0200

    Initial commit

---
+++ build.sh
@@ -0,0 +1,1 @@
+sudo rm -rf /home/natesworks/swiftos/out && sudo rm -rf /home/natesworks/swiftos/work && sudo mkarchiso -v -w /home/natesworks/swiftos/work -o /home/natesworks/swiftos/out /home/natesworks/swiftos
